cd10a5cbf1767540de4e2bc517cbe8727e8a80ab
varun <varun@DESKTOP-OP8GM6K>
2023-05-12 12:37:44 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+using namespace std;
+int main()
+{
+	ifstream file;
+	file.open("file.txt"); 
+	string s;
+	stringstream ss;
+	ss << file.rdbuf(); 
+	s = ss.str();
+	file.close();
+	int j = 1;
+	ofstream fileout;
+	fileout.open("fileout.txt");
+	for (int i=1;i<s.size();i++)
+	{
+		if (s[i] == s[i - 1])
+		{
+			j = j + 1;
+		}
+		else
+		{
+			fileout << s[i] << j;
+			j = 1;
+		}
+	}
+	fileout.close();
+}
