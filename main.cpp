49d9f8c6acee64493c697a08f4ecbc4e761b88a2
varunav123 <132358194+varunav123@users.noreply.github.com>
2023-05-31 01:07:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
 #include<iostream>
 #include<fstream>
 #include<sstream>
+#include<string>
 using namespace std;
-int main()
+string rleencoder(string& s)
 {
-	ifstream file;
-	file.open("file.txt"); 
-	string s;
-	stringstream ss;
-	ss << file.rdbuf(); 
-	s = ss.str();
-	file.close();
+	string s1 = "";
 	int j = 1;
-	ofstream fileout;
-	fileout.open("fileout.txt");
-	for (int i=0;i<s.size();i++)
+	for (int i = 0; i < s.size(); i++)
 	{
 		if (s[i] == s[i + 1])
 		{
@@ -22,9 +15,42 @@ int main()
 		}
 		else
 		{
-			fileout << s[i] << j;
-			j = 1;
+			if (j > 1)
+			{
+				string num = to_string(j);
+				s1 = s1 + s[i] + num;
+				/*fileout << s[i] << j;*/
+
+
+
+				j = 1;
+			}
+			else
+			{
+				s1 = s1+ s[i];
+				j = 1;
+			}
+
 		}
 	}
+	return s1;
+
+}
+
+int main()
+{
+
+	ofstream fileout;
+	fileout.open("fileout.txt");
+	ifstream file;
+	file.open("file.txt"); 
+	string s;
+	stringstream ss;
+	ss << file.rdbuf(); 
+	s = ss.str();
+	file.close();
+	string s1 = rleencoder(s);
+	cout << s1;
+	fileout << s1;
 	fileout.close();
 }
